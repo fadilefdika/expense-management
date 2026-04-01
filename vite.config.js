efe7e3cd841c8c0ee876503f010fe1a42d3fec9d
uvenven <fadilefd1102@gmail.com>
2026-04-01 14:59:14 +0700

    feat: add create.js and form.js to Vite build input configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["resources/sass/app.scss", "resources/js/app.js"],
+            input: [
+                "resources/sass/app.scss", 
+                "resources/js/app.js",
+                "resources/js/create.js",
+                "resources/js/form.js"
+            ],
             refresh: true,
             valetTls: false, // ⬅ TAMBAHKAN INI untuk cegah Valet
         }),
